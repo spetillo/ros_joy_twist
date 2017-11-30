e244d9e20dc2199227985500ddc048df9eea10bc
Stephanie Petillo <spetillo@whoi.edu>
2017-11-30 15:31:33 -0500

    joy twist working

--- src/joy_twist.cpp
+++ src/joy_twist.cpp
@@ -25,39 +25,94 @@
 //ros::geometry_msgs::Twist "/edumip/cmd";
 //ros::sensor_msgs::Joy "/joy";
 
-geometry_msgs::Twist vel_msg_;
+class TeleopRobot
+{
+public:
+  TeleopRobot();
 
-void joyCallback(const sensor_msgs::Joy::ConstPtr& msg){
-  vel_msg_.angular.z = msg->axes[0];
-  vel_msg_.linear.x = msg->axes[1];
-  ROS_INFO("Got axis values?");
+private:
+  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+
+  ros::NodeHandle nh_;
+
+  int linear_, angular_;
+  // double l_scale_, a_scale_;
+  ros::Publisher vel_pub_;
+  ros::Subscriber joy_sub_;
+
+};
+
+
+
+TeleopRobot::TeleopRobot():
+  linear_(4),
+  angular_(3)
+{
+
+  nh_.param("axis_linear", linear_, linear_);
+  nh_.param("axis_angular", angular_, angular_);
+  // nh_.param("scale_angular", a_scale_, a_scale_);
+  //nh_.param("scale_linear", l_scale_, l_scale_);
+
+
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("edumip/cmd", 1);
+
+
+  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopRobot::joyCallback, this);
+
 }
 
-int main(int argc, char** argv){
-  ros::init(argc, argv, "my_joy_twist");
 
-  ros::NodeHandle node;
+void TeleopRobot::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  geometry_msgs::Twist twist;
+  twist.angular.z = joy->axes[angular_];
+  twist.linear.x = joy->axes[linear_];
+  vel_pub_.publish(twist);
+}
 
-  ros::Subscriber sub = node.subscribe<sensor_msgs::Joy>("joy", 10, &joyCallback);
-  
-  ros::Publisher joy_cmd = node.advertise<geometry_msgs::Twist>("edumip/cmd", 10);
 
-  ros::Rate rate(10.0);
-  while (node.ok()){
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "teleop_robot_twist");
+  TeleopRobot teleop_robot;
 
-    // geometry_msgs::Twist vel_msg;
-    // vel_msg.linear.x = axes_[1];
-    // vel_msg.angular.z = axes_[0];
-    
-    // vel_msg.angular.z = 4.0 * atan2(transform.getOrigin().y(),
-    //                                 transform.getOrigin().x());
-    // vel_msg.linear.x = 0.5 * sqrt(pow(transform.getOrigin().x(), 2) +
-    //                               pow(transform.getOrigin().y(), 2));
-    joy_cmd.publish(vel_msg_);
-
-    rate.sleep();
-  }
   ros::spin();
+}
+///////////////////////////////////////
+// geometry_msgs::Twist vel_msg_;
 
-  return 0;
-};
+// void joyCallback(const sensor_msgs::Joy::ConstPtr& msg){
+//   vel_msg_.angular.z = msg->axes[0];
+//   vel_msg_.linear.x = msg->axes[1];
+//   ROS_INFO("Got axis values?");
+// }
+
+// int main(int argc, char** argv){
+//   ros::init(argc, argv, "my_joy_twist");
+
+//   ros::NodeHandle node;
+
+//   ros::Subscriber sub = node.subscribe<sensor_msgs::Joy>("joy", 10, &joyCallback);
+  
+//   ros::Publisher joy_cmd = node.advertise<geometry_msgs::Twist>("edumip/cmd", 10);
+
+//   ros::Rate rate(10.0);
+//   while (node.ok()){
+
+//     // geometry_msgs::Twist vel_msg;
+//     // vel_msg.linear.x = axes_[1];
+//     // vel_msg.angular.z = axes_[0];
+    
+//     // vel_msg.angular.z = 4.0 * atan2(transform.getOrigin().y(),
+//     //                                 transform.getOrigin().x());
+//     // vel_msg.linear.x = 0.5 * sqrt(pow(transform.getOrigin().x(), 2) +
+//     //                               pow(transform.getOrigin().y(), 2));
+//     joy_cmd.publish(vel_msg_);
+
+//     rate.sleep();
+//   }
+//   ros::spin();
+
+//   return 0;
+// };
