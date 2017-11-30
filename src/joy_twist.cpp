0d62bb69b471a8f38ddd8f784868e52fd1062ebf
Stephanie Petillo <spetillo@whoi.edu>
2017-11-30 15:39:37 -0500

    Code cleanup & corrected positive linear and angular directions for controller.

--- src/joy_twist.cpp
+++ src/joy_twist.cpp
@@ -5,25 +5,9 @@
 // Your node should assign joystick axis 1 to twist.linear.x, and joystick axis 0 to twisr.angular.z — BUT YOU CAN CHOOSE A DIFFERENT MAPPING IF YOU LIKE — you may need to change a sign in the assignment so that pushing the joystick forward makes twist.linear.x positive, and pushing the joystick to the right makes the twist.angular.z positive.
 
 
-
-//SUBSCRIBE: sensor_msgs/Joy joystick topic entitled “/joy”
-
-//PUBLISH:geometry_msgs/Twist topic named “/edumip/cmd”
-// Use ROS Timer callback function to publish the Twist messages at 10Hz
-
-//JOYSTICK
-// axis 1 to twist.linear.x
-// axis 0 to twist.angular.z
-// joystick forward makes twist.linear.x positive
-// joystick right makes the twist.angular.z positive
-
-
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
-
-//ros::geometry_msgs::Twist "/edumip/cmd";
-//ros::sensor_msgs::Joy "/joy";
 
 class TeleopRobot
 {
@@ -36,7 +20,6 @@ private:
   ros::NodeHandle nh_;
 
   int linear_, angular_;
-  // double l_scale_, a_scale_;
   ros::Publisher vel_pub_;
   ros::Subscriber joy_sub_;
 
@@ -45,19 +28,21 @@ private:
 
 
 TeleopRobot::TeleopRobot():
+  //JOYSTICK
+  // axis 4 to twist.linear.x
+  // axis 3 to twist.angular.z
   linear_(4),
   angular_(3)
 {
 
   nh_.param("axis_linear", linear_, linear_);
   nh_.param("axis_angular", angular_, angular_);
-  // nh_.param("scale_angular", a_scale_, a_scale_);
-  //nh_.param("scale_linear", l_scale_, l_scale_);
 
-
+  //PUBLISH:geometry_msgs/Twist topic named “/edumip/cmd”
+  // Use ROS Timer callback function to publish the Twist messages at 10Hz
   vel_pub_ = nh_.advertise<geometry_msgs::Twist>("edumip/cmd", 1);
 
-
+  //SUBSCRIBE: sensor_msgs/Joy joystick topic entitled “/joy”
   joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopRobot::joyCallback, this);
 
 }
@@ -66,7 +51,11 @@ TeleopRobot::TeleopRobot():
 void TeleopRobot::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
   geometry_msgs::Twist twist;
-  twist.angular.z = joy->axes[angular_];
+  //JOYSTICK
+  // joystick forward makes twist.linear.x positive
+  // joystick right makes the twist.angular.z positive
+
+  twist.angular.z = -joy->axes[angular_];
   twist.linear.x = joy->axes[linear_];
   vel_pub_.publish(twist);
 }
@@ -79,40 +68,3 @@ int main(int argc, char** argv)
 
   ros::spin();
 }
-///////////////////////////////////////
-// geometry_msgs::Twist vel_msg_;
-
-// void joyCallback(const sensor_msgs::Joy::ConstPtr& msg){
-//   vel_msg_.angular.z = msg->axes[0];
-//   vel_msg_.linear.x = msg->axes[1];
-//   ROS_INFO("Got axis values?");
-// }
-
-// int main(int argc, char** argv){
-//   ros::init(argc, argv, "my_joy_twist");
-
-//   ros::NodeHandle node;
-
-//   ros::Subscriber sub = node.subscribe<sensor_msgs::Joy>("joy", 10, &joyCallback);
-  
-//   ros::Publisher joy_cmd = node.advertise<geometry_msgs::Twist>("edumip/cmd", 10);
-
-//   ros::Rate rate(10.0);
-//   while (node.ok()){
-
-//     // geometry_msgs::Twist vel_msg;
-//     // vel_msg.linear.x = axes_[1];
-//     // vel_msg.angular.z = axes_[0];
-    
-//     // vel_msg.angular.z = 4.0 * atan2(transform.getOrigin().y(),
-//     //                                 transform.getOrigin().x());
-//     // vel_msg.linear.x = 0.5 * sqrt(pow(transform.getOrigin().x(), 2) +
-//     //                               pow(transform.getOrigin().y(), 2));
-//     joy_cmd.publish(vel_msg_);
-
-//     rate.sleep();
-//   }
-//   ros::spin();
-
-//   return 0;
-// };
